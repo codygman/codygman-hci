0b8471a00eb78cd3e7547d686e0e0baa11456da1
codygman <cody@codygman.dev>
2021-07-11 22:15:49 +0000

    pull in work tower host

---
+++ hosts/work-tower.nix
@@ -0,0 +1,38 @@
+{ suites, pkgs, ... }:
+{
+  imports = [
+    ./work-tower/configuration.nix
+  ] ++ suites.remotedev;
+
+  location.provider = "manual";
+  location.latitude = 32.7767;
+  location.longitude = -96.7970;
+
+  # services.wakeonlan.interfaces = [ { interface = "enp37s0"; method = "magicpacket";}];
+
+  services.redshift = {
+    enable = true;
+    brightness = {
+      day = "1.0";
+      night = "0.7";
+    };
+    temperature = {
+      day = 6500;
+      night = 3801;
+    };
+    extraOptions = [ "-v" ];
+  };
+
+
+  networking.hostName = "work-tower"; # Define your hostname.
+  networking.networkmanager = {
+    enable = true;
+    packages = [ pkgs.networkmanager_openvpn ];
+  };
+
+  programs.nm-applet = {
+    indicator = true;
+    enable = true;
+  };
+
+}
