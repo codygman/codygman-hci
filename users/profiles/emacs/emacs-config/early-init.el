c485cd6d2b6bea31ef0685e5440e1d3a8d8e0b83
codygman <cody@codygman.dev>
2021-07-11 21:23:32 +0000

    add emacs early and init

---
+++ users/profiles/emacs/emacs-config/early-init.el
@@ -0,0 +1,6 @@
+;; -*- lexical-binding: t -*-
+(push '(menu-bar-lines . 0) default-frame-alist) ;; remove mini menu
+(push '(tool-bar-lines . 0) default-frame-alist) ;; remove tool icon
+(push '(vertical-scroll-bars) default-frame-alist) ;; no scroll bar
+(setq inhibit-startup-screen t
+      inhibit-startup-echo-area-message t)
