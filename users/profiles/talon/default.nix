83baa5b4b89f0b1cee733f4e63f39d35e68fe5e2
codygman <cody@codygman.dev>
2021-07-11 04:54:47 +0000

    move talon stuff into own profile
    This is only the configuration and instructions to symlink it

---
+++ users/profiles/talon/default.nix
@@ -0,0 +1,5 @@
+{
+  # TODO get this symlinking working
+  # until then: ln -s ~/hci/users/profiles/talon/user/ ~/.talon/user
+  # home.file."~/.talon/user".source = config.lib.file.mkOutOfStoreSymlink ./user;  
+}
