5489f6a972e0812cb9d844596bed8795f5297e2a
codygman <cody@codygman.dev>
2021-07-25 15:47:19 -0500

    enable redshift on tower

--- hosts/tower.nix
+++ hosts/tower.nix
@@ -3,4 +3,22 @@
   imports = [
     ../up/tower/configuration.nix
   ] ++ suites.hci;
+
+  location.provider = "manual";
+  location.latitude = 32.7767;
+  location.longitude = -96.7970;
+  
+  services.redshift = {
+    enable = true;
+    brightness = {
+      day = "1.0";
+      night = "0.7";
+    };
+    temperature = {
+      day = 6500;
+      night = 3801;
+    };
+    extraOptions = [ "-v" ];
+  };
+
 }
